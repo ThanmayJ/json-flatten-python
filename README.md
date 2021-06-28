706153f72b98d0969dd9860ac2456baeeed59d02
Thanmay Jayakumar <69724164+ThanmayJ@users.noreply.github.com>
2021-06-28 16:47:49 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,66 @@
+# json-flatten-python
+A utility to FLATTEN and UNFLATTEN .json files in python
+
+All sample files have been taken from [here](https://filesamples.com/formats/json)
+
+**JSON**
+
+A .json file might look like this
+```
+data = {'people': [{'age': 28,
+         'firstName': 'Joe',
+         'gender': 'male',
+         'lastName': 'Jackson',
+         'number': '7349282382'},
+        {'age': 32,
+         'firstName': 'James',
+         'gender': 'male',
+         'lastName': 'Smith',
+         'number': '5678568567'},
+        {'age': 24,
+         'firstName': 'Emily',
+         'gender': 'female',
+         'lastName': 'Jones',
+         'number': '456754675'}]}
+
+```
+## FLATTEN
+
+Flattening it will give:
+```
+flattened = {'people.[0].age': 28,
+             'people.[0].firstName': 'Joe',
+             'people.[0].gender': 'male',
+             'people.[0].lastName': 'Jackson',
+             'people.[0].number': '7349282382',
+             'people.[1].age': 32,
+             'people.[1].firstName': 'James',
+             'people.[1].gender': 'male',
+             'people.[1].lastName': 'Smith',
+             'people.[1].number': '5678568567',
+             'people.[2].age': 24,
+             'people.[2].firstName': 'Emily',
+             'people.[2].gender': 'female',
+             'people.[2].lastName': 'Jones',
+             'people.[2].number': '456754675'}
+ ```
+## UNFLATTEN
+
+ Unflattening a flattened .json file will bring back the original structure.
+ ```
+unflattened = {'people': [{'age': 28,
+               'firstName': 'Joe',
+               'gender': 'male',
+               'lastName': 'Jackson',
+               'number': '7349282382'},
+              {'age': 32,
+               'firstName': 'James',
+               'gender': 'male',
+               'lastName': 'Smith',
+               'number': '5678568567'},
+              {'age': 24,
+               'firstName': 'Emily',
+               'gender': 'female',
+               'lastName': 'Jones',
+               'number': '456754675'}]}
+```
---
+++ json_flattener.py
@@ -0,0 +1,71 @@
+def flatten(data):
+    output = {}
+
+    def flatten_helper(data, idx):
+        if isinstance(data, dict):
+            keys = list(data.keys())
+            if keys:
+                for key in keys:
+                    flatten_helper(data[key], idx+key+'.')
+            else:
+                output[idx[:-1]] = data
+        elif isinstance(data, list):
+            if data:
+                for i in range(len(data)): 
+                    flatten_helper(data[i], idx+f'[{i}].')
+            else:
+                output[idx[:-1]] = []
+        else:
+            output[idx[:-1]] = data
+    
+    flatten_helper(data, '')
+    
+    return output
+
+def unflatten(data):
+    if isinstance(data, dict):
+        f_keys = list(data.keys())
+        if f_keys[0].split('.')[0][0] == '[' and f_keys[0].split('.')[0][-1] == ']':
+            output = []
+        else:
+            output = {}
+
+    def unflatten_helper(data, f_keys):
+        for f_key in f_keys:
+            val = data[f_key]
+            keys = f_key.split('.')
+            i = 0
+            x = output
+            while i < len(keys) - 1:
+                if isinstance(x, dict):
+                    try:
+                        x = x[keys[i]]
+                    except KeyError:
+                            if keys[i+1][0] == '[' and keys[i+1][-1] == ']':
+                                x[keys[i]] = []
+                            else:
+                                x[keys[i]] = {}
+                            x = x[keys[i]]
+                elif isinstance(x, list):
+                    if keys[i+1][0] == '[' and keys[i+1][-1] == ']':
+                        try:
+                            x = x[int(keys[i+1][1:-1])]
+                        except IndexError:
+                            x.append([])
+                            x = x[int(keys[i+1][1:-1])]
+                    else:
+                        try:
+                            x = x[int(keys[i][1:-1])]
+                        except IndexError:
+                            x.append({})
+                            x = x[int(keys[i][1:-1])]
+                i+=1
+            
+            if isinstance(x, list):
+                x.append(val) 
+            else:   
+                x[keys[-1]] = val
+    
+    unflatten_helper(data, f_keys)
+    
+    return output  
